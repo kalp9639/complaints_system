037a77bea8fc26da9c936118e6e71a67084a5b36
Kalp Patel <kp764@snu.edu.in>
2025-04-14 12:24:08 +0530

    fixes

--- notifications/admin.py
+++ notifications/admin.py
@@ -1,3 +1,11 @@
-from django.contrib import admin
+# notifications/admin.py
 
-# Register your models here.
+from django.contrib import admin
+from .models import Notification
+
+@admin.register(Notification)
+class NotificationAdmin(admin.ModelAdmin):
+    list_display = ('recipient', 'actor', 'verb', 'target', 'action_object', 'timestamp', 'unread')
+    list_filter = ('unread', 'timestamp', 'target_content_type') # Filter by read status, time, or target model type
+    search_fields = ('recipient__username', 'actor__username', 'verb') # Allow searching
+    readonly_fields = ('target', 'action_object') # Display GFKs read-only
